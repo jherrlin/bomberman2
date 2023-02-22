3a72e8403ed2aa94c997c1c4473a87b486361206
John Herrlin <jherrlin@gmail.com>
2023-02-22 08:05:13 +0100

    ktable

--- src/services/gateway2/src/main/kotlin/se/jherrlin/gateway/MyStream.kt
+++ src/services/gateway2/src/main/kotlin/se/jherrlin/gateway/MyStream.kt
@@ -3,10 +3,7 @@ package se.jherrlin.gateway
 import org.apache.kafka.common.serialization.Serdes
 import org.apache.kafka.streams.KeyValue
 import org.apache.kafka.streams.StreamsBuilder
-import org.apache.kafka.streams.kstream.Consumed
-import org.apache.kafka.streams.kstream.ForeachAction
-import org.apache.kafka.streams.kstream.KStream
-import org.apache.kafka.streams.kstream.Produced
+import org.apache.kafka.streams.kstream.*
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.annotation.EnableKafkaStreams
@@ -17,7 +14,7 @@ import org.springframework.kafka.support.serializer.JsonSerde
 class MyStream {
     val sysout1 = ForeachAction { key: String?, value: String -> println("After: key ${key}  value ${value}") }
     val stringSerde = Serdes.String()
-        val sysout = { key: Any?, value: Any -> println("Before: key $key, value $value") }
+    val sysout = { key: Any?, value: Any -> println("Before: key $key, value $value") }
 
     @Bean
     fun kStream(streamsBuilder: StreamsBuilder): KStream<String, String> {
@@ -29,6 +26,15 @@ class MyStream {
             .to("s1_uppercase", Produced.with(stringSerde, stringSerde))
         return stream
     }
+
+    @Bean
+    fun kTable(streamsBuilder: StreamsBuilder): KTable<String, Long> =
+        streamsBuilder
+            .table("s2", Consumed.with(stringSerde, stringSerde))
+            .groupBy({key, value-> KeyValue.pair(key, value)},
+                Grouped.with(stringSerde, stringSerde)
+            )
+            .count(Materialized.`as`("counting-store"))
 
     private fun uppercaseValue(key: String, value: String): KeyValue<String?, String?> {
         return KeyValue(key, value.uppercase())
